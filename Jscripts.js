919cb8b3a10a0c0da316794dcae4f5d590842926
Caleb Canada <calebcanada13@gmail.com>
2018-07-31 14:55:27 -0500

    Check in

---
+++ Jscripts.js
@@ -0,0 +1,14 @@
+function main() {
+    var number =21;
+    var number1 =9;
+    var number2 =35;
+
+    alert(sumNumber(number, number1, number2));
+}
+function sumNumber(num, num1, num2){
+    var results = num + num1 + num2;
+
+    return results;
+}
+
+main();
---
+++ Jscripts1.js
@@ -0,0 +1,19 @@
+function main() {
+    var num =14;
+    var num1 =7;
+
+    alert(product(num, num1));
+    alert(quotient(num, num1))
+}
+function product(num, num1) {
+    var result = num * num1;
+
+    return result
+}
+function quotient(num, num1) {
+    var results = num / num1;
+
+    return results
+}
+
+main();
